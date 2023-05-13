c3f9e0dbc72d42542f4cbecf94fa84c2759bb4ef
bebyshaffira12 <beby26shaffira@gmail.com>
2023-05-13 19:21:44 +0700

    Innitial commit

---
+++ app/src/main/java/com/example/pertemuan06sqlite/DBModel.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan06sqlite
+
+data class DBModel  (
+    var email: String = "",
+    var pass: String = "",
+    var username: String = "",
+    var fullname: String = ""
+)
